f0b13c7a87947cfb7563f22d5450ec8929e3ddc4
evansamenorfe <evansamenorfe@gmail.com>
2023-01-06 15:28:35 +0000

    Conditional statement

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your score in percentage? ");
+        string UserInput = Console.ReadLine();
+        int score = int.Parse(UserInput);
+
+        string letter = "";
+
+        if (score >= 90)
+        {
+            letter = "A";
+        }
+        else if ( score >= 80)
+        {
+            letter = "B";
+        }
+        else if (score >= 70)
+        {
+            letter = "C";
+        }
+        else if (score >= 60)
+        {
+            letter ="D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade letter is {letter}");
+
+        if (score >= 70)
+        {
+            Console.WriteLine("Congratulation, You have passed the class!!!!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you need to work harder to be able to pass this class.");
+        }
+
+
+
     }
 }
